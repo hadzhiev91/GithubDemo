39f97778c405ef73387a2c3c108aed7ace101782
Georgi <georgehadzhiev@gmail.com>
2019-12-17 14:32:19 +0200

    solved number printing

---
+++ PrintNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrintNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1);
+            Console.WriteLine(11);
+            Console.WriteLine(1001);
+        }
+    }
+}
